70fd8c37d26adeb3180f4c2714a3d3f9e308121d
Nick <akulinnikolajj@gmail.com>
2017-03-23 20:08:03 +0300

    мелкие фиксы

--- Parser.DAL/Migrations/Configuration.cs
+++ Parser.DAL/Migrations/Configuration.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity.Migrations;
 
 namespace Parser.DAL.Migrations
 {
@@ -12,6 +7,11 @@ namespace Parser.DAL.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Parser.DAL.ParserContext context)
+        {
+            
         }
     }
 }
